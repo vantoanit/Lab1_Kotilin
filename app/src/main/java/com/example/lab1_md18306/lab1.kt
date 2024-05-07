18bf85390f2843ebd406193ad82b5485cb6f4c0b
văn toàn <toan21851@gmail.com>
2024-05-07 10:59:23 +0700

    first update

---
+++ app/src/main/java/com/example/lab1_md18306/lab1.kt
@@ -0,0 +1,37 @@
+package com.example.lab1_md18306
+
+fun main (){
+
+
+    print("Nguyen Van Toan - PH35132\n")
+    print("============\n")
+    println("Quanh năm buôn bán ở mom sông")
+    println("Nuôi đủ năm con với một chồng")
+    println("\tlặn lội thân cờ khi quãng vắng")
+    println("\teo sèo mặt nước buổi đò đồng")
+    println("Một duyên hai nợ âu đành phận")
+    println("Năm nắng mười mưa há chẳng công")
+    println("\tCha mẹ thói đời \"ăn ở bạc\"")
+    println("\tCó chồng hờ hững cũng như không")
+    println("...................\n")
+
+
+    println("===============================")
+    println("Bai 2:")
+    var a = 0.0
+    var b = 0.0
+    println("Nhap a:")
+    var s = readLine()
+    if(s != null) a = s.toDouble()
+    println("Nhap b:")
+    s = readLine()
+    if(s != null) b = s.toDouble()
+    println("Tong:" +(a+b))
+    println("Hieu:" +(a-b))
+    println("Tich:" +(a*b))
+    println("Thuong:" +(a/b))
+
+}
+
+class lab1 {
+}
