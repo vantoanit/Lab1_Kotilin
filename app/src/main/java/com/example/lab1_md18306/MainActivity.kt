7c9a4ee4c1f216f7974f0ff9626b52b449d81e7d
văn toàn <toan21851@gmail.com>
2024-05-16 15:25:07 +0700

    update buoi 4

--- app/src/main/java/com/example/lab1_md18306/MainActivity.kt
+++ app/src/main/java/com/example/lab1_md18306/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.lab1_md18306
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab1_md18306.ui.theme.Lab1_md18306Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +33,46 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab1_md18306Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    GetLayout(title = "Trang chu")
                 }
             }
         }
         }
+
+@Preview
+@Composable
+fun GetLayout (title: String = "Trang chu") {
+    var count by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GetTextTiel(title = count.toString())
+        Button(onClick = {
+            count++
+        }) {
+            Text(text = "Click me")
+
+        }
+
+    }
+
+}
+
+@Composable
+fun GetTextTiel(title: String){
+    Text(
+        text = title,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        fontSize = 30.sp,
+        color = Color.Blue,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Composable
